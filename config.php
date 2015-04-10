e1d99d1e6fae43e5fd9ab252933dc5eb961a5bc8
mosvits <mosvits@gmail.com>
2015-04-10 15:12:38 +0300

    Access to database

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 10.04.2015
+ * Time: 15:09
+ */
+$host = "eshowcas.mysql.ukraine.com.ua";
+$user = "eshowcas_work";
+$pasw = "Work_RTF";
+$dbname = "eshowcas_work";
+
+
+
+?>
